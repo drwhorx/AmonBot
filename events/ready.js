0acca3591723864db8082976d4e47f4b1c639870
drwhorx <drwhorx@gmail.com>
2019-06-17 22:11:54 -0400

    recached

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = async client => {
-  client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
-  client.user.setActivity(`on ${client.guilds.size} servers for ${client.users.size} users`, {type: "PLAYING"});
-};
